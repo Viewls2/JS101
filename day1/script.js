57af1f050e779ca370cd408d5571b9baeb10ee59
Viewls2 <mahasamut.kh@gmail.com>
2023-03-09 16:15:45 +0700

    add(2): add file about variable

---
+++ day1/script.js
@@ -0,0 +1,6 @@
+function Hello() {
+    // alert("Hello, world");
+    // console.log("The button has been clicked.");//
+    // document.getElementById("text").innerHTML = "The button has been clicked";
+    window.prompt("The button has been clicked.","text?");
+}
