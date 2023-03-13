6475d5c0534469fe7c91faef17188d268d4234fb
Viewls2 <mahasamut.kh@gmail.com>
2023-03-13 15:11:44 +0700

    add ternary practice file.

---
+++ practice/ternary.js
@@ -0,0 +1,10 @@
+const password = 'Password1234';
+
+password.length >= 12 ? console.log('Password is very strong, Welcome!')  
+: password.length >= 8 ? console.log('Welcome!')
+: console.log('SORRY!!, password is too short.')
+
+
+password.length >= 124 ? console.log('Password is very strong, Welcome!')  
+: console.log('SORRY!!, password is too short.')
+
